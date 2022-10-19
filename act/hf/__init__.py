59ca53fe1e65a79ceb1ff635a45f00293352dcb8
AntoineSimoulin <antoine.simoulin@gmail.com>
2022-10-19 10:15:18 -0500

    :bugs: fix init

--- act/hf/__init__.py
+++ act/hf/__init__.py
@@ -1,3 +1,3 @@
-from configuration_albert_act import AlbertActConfig
-from modeling_albert_act_tf import TFAlbertActModel
-from modeling_albert_act import AlbertActModel
+from .configuration_albert_act import AlbertActConfig
+from .modeling_albert_act_tf import TFAlbertActModel
+from .modeling_albert_act import AlbertActModel
